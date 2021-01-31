51e3f56e5e54a6a0971cece2e0335f6aca59bdbd
Matthew Kennedy <m.kennedy@me.com>
2021-01-31 20:26:39 +0000

    Update

--- app/controllers/spree/contact_us/contacts_controller.rb
+++ app/controllers/spree/contact_us/contacts_controller.rb
@@ -1,13 +1,18 @@
 class Spree::ContactUs::ContactsController < Spree::StoreController
-  helper 'spree/products'
+  helper "spree/products"
   def create
     @contact = Spree::ContactUs::Contact.new(params[:contact_us_contact])
 
-    if @contact.save
+    if NewGoogleRecaptcha.human?(
+      params[:new_google_recaptcha_token],
+      "checkout",
+      NewGoogleRecaptcha.minimum_score,
+      @contact
+    ) && @contact.save
       if Spree::ContactUs::Config.contact_tracking_message.present?
         flash[:contact_tracking] = Spree::ContactUs::Config.contact_tracking_message
       end
-      redirect_to(spree.root_path, notice: Spree.t('spree_contact_us.notices.success'))
+      redirect_to(spree.root_path, notice: Spree.t("spree_contact_us.notices.success"))
     else
       render :new
     end
@@ -18,7 +23,8 @@ class Spree::ContactUs::ContactsController < Spree::StoreController
     @taxonomies = Spree::Taxonomy.includes(root: :children)
   end
 
-  def meta_data_tags; end
+  def meta_data_tags
+  end
   helper_method :meta_data_tags
 
   private
