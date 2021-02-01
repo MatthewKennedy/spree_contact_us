8fc36b04a95b4af844133c2595a75e5015d891bc
Matthew Kennedy <m.kennedy@me.com>
2021-02-01 13:08:46 +0000

    Update

--- app/overrides/contact_us_in_header.rb
+++ app/overrides/contact_us_in_header.rb
@@ -4,7 +4,5 @@ Deface::Override.new(
   virtual_path: "spree/layouts/spree_application",
   name: "add_js_to_head",
   insert_bottom: "[data-hook='inside_head']",
-  text: "<%= yield :recaptcha_js %>
-         <meta name='turbolinks-cache-control' content='no-cache'>
-         <meta name='turbolinks-visit-control' content='reload'>"
+  text: "<%= yield :recaptcha_js %>"
 )
